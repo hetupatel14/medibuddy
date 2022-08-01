ff436bd47c3cb40d7469059a6c5da1fb30f08b55
Bhavya Parekh <bhavyavilinparekh@gmail.com>
2022-08-01 13:53:39 +0530

    navigated the login screen to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './screens/homescreen.dart';
+import './screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
